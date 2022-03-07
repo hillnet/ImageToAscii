b503f251bb6eb8c6a2059a25d7afd30f14a48110
ubuntu <matt-hillery@hotmail.com>
2022-03-07 17:00:38 +1000

    Optimised loops

--- python/ascii.py
+++ python/ascii.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 from math import floor
-
 from PIL import Image
 
 
 def main(image):
-
     # image.show()
     w, h = image.size
     pixels = list(image.getdata())
@@ -15,31 +13,30 @@ def main(image):
         print("Pixels is not RGB. Given pixel data = " + str(pixels[0]))
         return
 
-    average_colour_pixels = []
-
-    for pixel in pixels:
-        average_colour = sum(pixel) / len(pixel)  # Getting the average value from the RBG values
-        average_colour_pixels.append(average_colour)
-
-    density_chars = "Ñ@#W$9876543210?!abc;:+=-,._      "
+    density_chars = "Ñ@#W$9876543210?!abc;:+=-,._        "
     # density_chars = "_.,-=+:;cba!?0123456789$W#@Ñ   "
     density_chars_len = len(density_chars)
     pixel_index_count = 0
+
     string_buffer = []
-    for y in range(h):
-        for x in range(w):
-            average_colour_pixel = average_colour_pixels[pixel_index_count]
+    for pixel in pixels:
+        average_colour = sum(pixel) / len(pixel)  # Getting the average value from the RBG values
 
-            density_index = floor((average_colour_pixel * density_chars_len) / 255)
+        density_index = floor((average_colour * density_chars_len) / 255)
+        string_buffer.append(density_chars[density_index - 1])
 
-            string_buffer.append(density_chars[density_index - 1])
-            pixel_index_count += 1
-        print("".join(string_buffer))
-        string_buffer.clear()
+        if pixel_index_count == w - 1:
+            print("".join(string_buffer))
+            string_buffer.clear()
+            pixel_index_count = 0
+            continue
+
+        pixel_index_count += 1
 
 
 if __name__ == "__main__":
     import sys
+
     if len(sys.argv) != 2:
         print("Specify image")
         print("Usage: python3 ascii.py blake_oakfield.jpg")
